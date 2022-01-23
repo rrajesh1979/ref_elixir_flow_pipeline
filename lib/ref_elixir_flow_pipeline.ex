d3aaac10fffb810dd007ec3b11b35c50aef07150
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-01-22 19:05:27 -0500

    :rocket: Flow based pipeline draft

--- lib/ref_elixir_flow_pipeline.ex
+++ lib/ref_elixir_flow_pipeline.ex
@@ -1,18 +1,270 @@
-defmodule RefElixirFlowPipeline do
+defmodule RefElixir.FlowPipeline do
   @moduledoc """
-  Documentation for `RefElixirFlowPipeline`.
+  Documentation for `RefElixir.FlowPipeline`.
   """
 
-  @doc """
-  Hello world.
+  require Logger
+  require Flow
+  alias NimbleCSV.RFC4180, as: CSV
 
-  ## Examples
+  require Application
+  @experian_endpoint Application.compile_env(:ref_elixir_flow_pipeline, :experian_endpoint)
+  @equifax_endpoint Application.compile_env(:ref_elixir_flow_pipeline, :equifax_endpoint)
+  @aml_check_endpoint Application.compile_env(:ref_elixir_flow_pipeline, :aml_check_endpoint)
+  @fraud_check_endpoint Application.compile_env(:ref_elixir_flow_pipeline, :fraud_check_endpoint)
+  @account_balance_endpoint Application.compile_env(
+                              :ref_elixir_flow_pipeline,
+                              :account_balance_endpoint
+                            )
 
-      iex> RefElixirFlowPipeline.hello()
-      :world
+  def main(args \\ []) do
+    Logger.info("Starting RefElixir.BasicPipeline")
+    Logger.info("Arguments: #{args}")
 
-  """
-  def hello do
-    :world
+    args
+    |> parse_args()
+    |> response()
+  end
+
+  defp parse_args(args) do
+    {opts, file_path, _} =
+      args
+      |> OptionParser.parse(switches: [file: :boolean])
+
+    {opts, List.to_string(file_path)}
+  end
+
+  defp response({opts, file_path}) do
+    case opts[:file] do
+      true ->
+        Logger.info("File path: #{inspect(file_path)}")
+        file_path
+
+      _ ->
+        Logger.error("
+          File path is not specified
+          usage:
+            ./ref_elixir_basic_pipeline {options} arg1 arg2 ...
+          ")
+        raise "Error"
+    end
+  end
+
+  def process_flow(file_path) do
+    read_feed_files(file_path)
+    |> Flow.from_enumerables(max_demand: 10)
+    |> Flow.partition(max_demand: 10, stages: 5)
+    |> Flow.map(&parse_record/1)
+    |> Flow.map(&experian_check/1)
+    |> Flow.map(&equifax_check/1)
+    |> Flow.map(&aml_check/1)
+    |> Flow.map(&fraud_check/1)
+    |> Flow.map(&account_balance_check/1)
+    |> Flow.map(&credit_decision/1)
+    |> Enum.to_list()
+  end
+
+  def read_feed_files(file_path) do
+    file_streams =
+      for file <- File.ls!(file_path) do
+        File.stream!(file_path <> "/" <> file, read_ahead: 100_000)
+        |> Stream.drop(1)
+      end
+
+    file_streams
+  end
+
+  def parse_record(row) do
+    Logger.info("Parsing record: #{row}")
+    [row] = CSV.parse_string(row, skip_headers: false)
+
+    %{
+      request_id: Enum.at(row, 0),
+      name: Enum.at(row, 1),
+      credit_requested: Enum.at(row, 2),
+      requested_date: Enum.at(row, 3),
+      location: Enum.at(row, 4),
+      status: "NEW_REQUEST",
+      activity_log: ["REQUEST_CREATED"]
+    }
+  end
+
+  def experian_check(request) do
+    Logger.info("Request for Experian Check: #{inspect(request)}")
+
+    response =
+      case HTTPoison.get!(@experian_endpoint) do
+        %HTTPoison.Response{status_code: 200} ->
+          %{
+            request_id: request.request_id,
+            request_type: "Experian Check",
+            status_code: 200
+          }
+
+        _ ->
+          %{
+            request_id: request.request_id,
+            request_type: "Experian Check",
+            status_code: :error
+          }
+      end
+
+    Logger.info("Response Experian Check: #{inspect(response)}")
+
+    request_processed = %{
+      request
+      | status: "EXPERIAN_CHECK_DONE",
+        activity_log: Enum.concat(request.activity_log, ["EXPERIAN_CHECK"])
+    }
+
+    request_processed
+  end
+
+  def equifax_check(request) do
+    Logger.info("Request for Equifax Check: #{inspect(request)}")
+
+    response =
+      case HTTPoison.get!(@equifax_endpoint) do
+        %HTTPoison.Response{status_code: 200} ->
+          %{
+            request_id: request.request_id,
+            request_type: "Equifax Check",
+            status_code: 200
+          }
+
+        _ ->
+          %{
+            request_id: request.request_id,
+            request_type: "Equifax Check",
+            status_code: :error
+          }
+      end
+
+    Logger.info("Response from Equifax Check: #{inspect(response)}")
+
+    request_processed = %{
+      request
+      | status: "EQUIFAX_CHECK_DONE",
+        activity_log: Enum.concat(request.activity_log, ["EQUIFAX_CHECK"])
+    }
+
+    request_processed
+  end
+
+  def aml_check(request) do
+    Logger.info("Request for AML Check: #{inspect(request)}")
+
+    response =
+      case HTTPoison.get!(@aml_check_endpoint) do
+        %HTTPoison.Response{status_code: 200} ->
+          %{
+            request_id: request.request_id,
+            request_type: "AML Check",
+            status_code: 200
+          }
+
+        _ ->
+          %{
+            request_id: request.request_id,
+            request_type: "AML Check",
+            status_code: :error
+          }
+      end
+
+    Logger.info("Response from AML Check: #{inspect(response)}")
+
+    request_processed = %{
+      request
+      | status: "AML_CHECK_DONE",
+        activity_log: Enum.concat(request.activity_log, ["AML_CHECK"])
+    }
+
+    request_processed
+  end
+
+  def fraud_check(request) do
+    Logger.info("Request for Fraud Check: #{inspect(request)}")
+
+    response =
+      case HTTPoison.get!(@fraud_check_endpoint) do
+        %HTTPoison.Response{status_code: 200} ->
+          %{
+            request_id: request.request_id,
+            request_type: "Fraud Check",
+            status_code: 200
+          }
+
+        _ ->
+          %{
+            request_id: request.request_id,
+            request_type: "Fraud Check",
+            status_code: :error
+          }
+      end
+
+    Logger.info("Response from Fraud Check: #{inspect(response)}")
+
+    request_processed = %{
+      request
+      | status: "FRAUD_CHECK_DONE",
+        activity_log: Enum.concat(request.activity_log, ["FRAUD_CHECK"])
+    }
+
+    request_processed
+  end
+
+  def account_balance_check(request) do
+    Logger.info("Request for Account Balance Check: #{inspect(request)}")
+
+    response =
+      case HTTPoison.get!(@account_balance_endpoint) do
+        %HTTPoison.Response{status_code: 200} ->
+          %{
+            request_id: request.request_id,
+            request_type: "Account Balance Check",
+            status_code: 200
+          }
+
+        _ ->
+          %{
+            request_id: request.request_id,
+            request_type: "Account Balance Check",
+            status_code: :error
+          }
+      end
+
+    Logger.info("Response from Account Balance Check: #{inspect(response)}")
+
+    request_processed = %{
+      request
+      | status: "ACCOUNT_BALANCE_CHECK_DONE",
+        activity_log: Enum.concat(request.activity_log, ["ACCOUNT_BALANCE_CHECK"])
+    }
+
+    request_processed
+  end
+
+  def credit_decision(request) do
+    Logger.info("Final decision on credit request: #{inspect(request)}")
+
+    final_decision =
+      case request.status do
+        "ACCOUNT_BALANCE_CHECK_DONE" ->
+          "APPROVED"
+
+        _ ->
+          "REJECTED"
+      end
+
+    request_processed = %{
+      request
+      | status: final_decision,
+        activity_log: Enum.concat(request.activity_log, ["CREDIT_DECISION"])
+    }
+
+    request_processed
   end
 end
+
+# Commands
+# RefElixir.FlowPipeline.process_flow("/Users/rajesh/Learn/elixir/ref_elixir_flow_pipeline/priv/data")
--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule RefElixirFlowPipeline.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript(),
+      # Docs
+      name: :ref_elixir_flow_pipeline,
+      source_url: "https://github.com/rrajesh1979/ref_elixir_flow_pipeline",
+      docs: [
+        # The main page in the docs
+        main: "RefElixir.FlowPipeline",
+        # logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -24,6 +34,27 @@ defmodule RefElixirFlowPipeline.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # Elixir documentation generation tool
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+
+      # Dev / Test Libraries
+      # Static code analysis tool with a focus on code consistency and teaching.
+      {:credo, "~> 1.6"},
+      # Dialyzer is a static analysis tool
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+
+      # Library to make HTTP calls
+      {:httpoison, "~> 1.8"},
+
+      # Library to parse CSV files
+      {:nimble_csv, "~> 1.2"},
+
+      # Library to build flow pipeline
+      {:flow, "~> 1.1"}
     ]
+  end
+
+  defp escript do
+    [main_module: RefElixir.FlowPipeline]
   end
 end
